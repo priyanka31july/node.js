383f39240c6427d72f73c63a79c61ba37be2295f
priyanka31july <priyankad4040@gmail.com>
2023-12-13 17:34:50 +0530

    ass2

---
+++ ass1_kickstart_with_web/ass2_task1.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+
+app.use((req,res,next)=>{
+    console.log("in the middleware");
+    next();
+})
+
+app.use((req,res,next)=>{
+    console.log("in another middleware");
+    res.send('<h1>hello from Express</h1>');
+});
+
+app.listen(4000)
