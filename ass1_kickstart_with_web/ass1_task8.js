234ea291b81162793a098fc89687ed4d0090c1aa
priyanka31july <priyankad4040@gmail.com>
2023-12-20 23:51:58 +0530

    ass2_task8.1

--- ass1_kickstart_with_web/ass1_task8.js
+++ ass1_kickstart_with_web/ass1_task8.js
@@ -1,43 +1,69 @@
 const http = require('http');
-
 const fs = require('fs');
 
-const server = http.createServer((req, res) => {
+const requestHandler = (req, res) => {
+    const url = req.url;
+    const method = req.method;
 
-const url = req.url;
-const method = req.method;
+    if (url === '/') {
+        fs.readFile('message.txt', { encoding: 'utf-8' }, (err, data) => {
+            if (err) {
+                console.log(err);
+                res.statusCode = 500; 
+                res.end();
+            } else {
+              
+                res.setHeader('Content-Type', 'text/html');
+                res.write('<html>');
+                res.write('<head><title>Enter Message</title></head>');
+                
+                if (data) {
+                    res.write(`<pre>${data}</pre>`); // Printing data as is
+                }
+                
+           
+                res.write('<form action="/message" method="POST"><input type="text" name="message">');
+                res.write('<button type="send">send</button></form>');
+                res.write('</html>');
+                return res.end();
+            }
+        });
+    } else if (url === '/message' && method === 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            body.push(chunk) ;
+        });
 
-if (url === '/') {
-  res.write('<html>');
-  res.write('<head><title>Enter Message</title></head>');
-   res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Submit</button></form></body>');
-  
-  res.write('</html>');
-  return res.end();
-}
-if (url === '/message' && method==='POST') {
-   const body=[];
-   req.on('data',(chunk)=>{
-   console.log(chunk);
-   body.push(chunk);
-   });
-   req.on('end', ()=>{
-   const parsedBody=Buffer.concat(body).toString();
-   const message=parsedBody.split('=')[1];
-   console.log(parsedBody)
-   fs.writeFileSync('message.txt',message)
-   });
-   res.statusCode=302;
-   res.setHeader('Location', '/');
-   return res.end();
-   }
+        req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split('=')[1];
+            
+            fs.appendFile('message.txt', message + '\n', (err) => {
+                if (err) {
+                    console.log(err);
+                    res.statusCode = 500; 
+                    res.end();
+                } else {
+                    console.log('Inside fs.writeFile');
+                    res.statusCode = 302;
+                    res.setHeader('Location', '/');
+                    return res.end();
+                }
+            });
+        });
+    } else {
+        res.setHeader('Content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body><h1>Hello from Node.js</h1></body>');
+        res.write('</html>');
+        res.end();
+    }
+};
 
-res.setHeader('Content-Type', 'text/html');
-res.write('<html>');
-res.write('<head><title>My first page</title></head>');
-res.write('<body><h1>Hello from my Node.js server</h1></body>');
-res.write('</html>');
-res.end();
+const server = http.createServer(requestHandler);
+
+server.listen(3000, () => {
+    console.log('Server is listening on port 3000');
 });
 
-server.listen(3000);
