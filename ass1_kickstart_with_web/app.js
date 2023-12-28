70eab264de26db785e72fade5461b16bd391cc33
priyanka31july <priyankad4040@gmail.com>
2023-12-28 17:25:42 +0530

    ass2_task4

--- ass1_kickstart_with_web/app.js
+++ ass1_kickstart_with_web/app.js
@@ -1,18 +1,65 @@
-
+ 
 const express = require('express');
+const fs = require('fs');
 const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.get('/', (req, res) => {
+    fs.readFile('messages.txt', 'utf8', (err, data) => {
+        if (err) { 
+            console.log(err);
+            data = 'No chat exists';
+        }
 
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use('/admin', adminRoutes);
-app.use('/', shopRoutes);
-
-app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not found</h1>');
+        res.send(`
+            <div>${data}</div>
+            <form action="/" method="POST">
+                <input type="text" name="message" placeholder="Enter your message">
+                <input type="hidden" name="username" value="${req.query.username || ''}">
+                <button type="submit">Send</button>
+            </form>
+        `);
+    });
 });
 
-app.listen(3000) 
+app.post('/', (req, res) => {
+    const { username, message } = req.body;
+    fs.appendFile('messages.txt', `${username}: ${message}\n`, (err) => {
+        if (err) {
+            console.log(err);
+        }
+        res.redirect('/');
+    });
+});
+
+app.get('/login', (req, res) => {
+    res.send(`
+        <form action="/login" method="POST">
+            <input type="text" name="username" placeholder="Enter your username">
+            <button type="submit">Login</button>
+        </form>
+    `);
+});
+
+app.post('/login', (req, res) => {
+    const { username } = req.body;
+   
+    res.send(`
+        <script>
+            localStorage.setItem('username', '${username}');
+            window.location.href = '/';
+        </script>
+    `);
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
+
+
+
+
