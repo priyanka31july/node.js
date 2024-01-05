6ff2438f972e8b971099b250435daef15f483648
priyanka31july <priyankad4040@gmail.com>
2024-01-05 17:03:22 +0530

    ass2_task7

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,3 @@
-
 const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
@@ -18,14 +17,16 @@ exports.postAddProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  const products = Product.fetchAll();
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
-    activeShop: true,
-    productCSS: true
+  Product.fetchAll(products => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+      activeShop: true,
+      productCSS: true
+    });
   });
 };
+
 
